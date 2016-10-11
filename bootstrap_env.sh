4463146ae82a8f6b0226c91ad3bab17e1bc02f39
jdfless <jdfless@noemail.com>
2016-10-11 15:28:30 -0700

    typo in copy to all vimrc

--- bootstrap_env.sh
+++ bootstrap_env.sh
@@ -4,7 +4,7 @@ cp -r .vim ~/
 cp .hgrc ~/
 cp .vimrc ~/
 
-helping_scripts/copy_to_all.sh /highland/.vim --same --no-prompt
+helping_scripts/copy_to_all.sh /highland/.vimrc --same --no-prompt
 helping_scripts/copy_to_all.sh /highland/.hgrc --same --no-prompt
 helping_scripts/copy_to_all.sh /highland/.vim/colors/monokai.vim --same --no-prompt
 helping_scripts/copy_to_all.sh /highland/.vim/syntax/puppet.vim --same --no-prompt
