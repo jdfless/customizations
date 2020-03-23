1904c68b428ed1dac00413e76a45e23636e8893c
Jonathan Flessner <jflessner@skytap.com>
2020-03-23 19:06:33 +0000

    boostrap: change perms on key and remote on repo

--- bootstrap_env.sh
+++ bootstrap_env.sh
@@ -27,6 +27,8 @@ helping_scripts/copy_to_all.sh /highland/.vim/syntax/puppet.vim --same --no-prom
 if [ ! -f ~/.ssh/jdf_skytap_github ]; then
   echo "creating new github ssh key"
   ssh-keygen -f ~/.ssh/jdf_skytap_github -t rsa -b 4096 -C "jflessner@skytap.com" -N ''
+  sudo chown 0600 /highland/.ssh/jdf_skytap_github
+  git remote set-url origin git@github.com:jdfless/customizations.git
 fi
 
 git config --global user.email "jflessner@skytap.com"
